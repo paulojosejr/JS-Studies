ac0f0b07a3e9f9fd62eafd140a509e8fc0cba82a
Paulo Araújo <paulojosejr95@gmail.com>
2020-07-14 00:07:47 -1100

    commit

---
+++ fundamentals/strings.js
@@ -0,0 +1,15 @@
+const school = "Coder"
+
+console.log(school.charAt(4))
+console.log(school.charAt(5))
+console.log(school.charCodeAt(3))
+console.log(school.indexOf('e'))
+
+console.log(school.substring(1))
+console.log(school.substring(0,3))
+
+console.log('School '.concat(school).concat("!"))
+console.log('School ' + school + "!")
+console.log(school.replace('e', 3))
+
+console.log('David,Michael,Ariana'.split(','))
---
+++ fundamentals/templateString.js
@@ -0,0 +1,12 @@
+const name = 'Ariana'
+const concatenation = 'Hello ' + name + '!'
+const template = `
+Hello
+${name}!
+`
+console.log(concatenation, template)
+
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = text => text.toUpperCase()
+console.log(`Yo... ${up('caution')}!`)
