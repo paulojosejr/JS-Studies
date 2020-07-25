3f5b9a12b9be1d95826c179c6b2998364bb8b0fb
Paulo Araújo <paulojosejr95@gmail.com>
2020-07-24 21:33:30 -1100

    commit

---
+++ studies/object/oo.js
@@ -0,0 +1,21 @@
+//NON-EXECUTABLE CODE
+
+//Procedural
+processing(value1, value2, value3)
+
+// OO
+object = {
+    value1,
+    value2,
+    value3,
+    processing(){
+        //...
+    }
+}
+object.processing()
+
+//Important pricipios
+// 1. abstraction
+// 2. encapsulation
+// 3. inheritance (prototype)
+// 4. polymorphism
