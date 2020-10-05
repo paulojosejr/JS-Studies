b1d8051553bacb66ac7b8920ab7272eee7dfa6c9
Paulo Araújo <paulojosejr95@gmail.com>
2020-10-05 09:39:37 +0100

    crud project components and style(css grid)

---
+++ react-JS/crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import './Main.css'
+import React from 'react'
+import Header from './Header'
+
+export default porps =>
+    <React.Fragment>
+        <Header />
+        <main className="content">
+            Content
+        </main>
+    </React.Fragment>
