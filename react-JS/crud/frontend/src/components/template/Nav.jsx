fa277efc523b208d4970d90c34a59aabd066e198
Paulo Araújo <paulojosejr95@gmail.com>
2020-10-06 10:58:18 +0100

    crud project components

--- react-JS/crud/frontend/src/components/template/Nav.jsx
+++ react-JS/crud/frontend/src/components/template/Nav.jsx
@@ -3,5 +3,12 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navigation
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"> </i> Start
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Users
+            </a>
+        </nav>
     </aside>
