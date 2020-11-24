7d9180646a118325eb4e5791c77e693286e5767b
Paulo Araújo <paulojosejr95@gmail.com>
2020-11-24 17:16:08 +0000

    httpRequest axios

--- api/httpRequest/axios.js
+++ api/httpRequest/axios.js
@@ -1,9 +1,27 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {};
+const getData = () => {
+    axios.get('https://reqres.in/api/users').then(response => {
+        console.log(response)
+    })
+}
 
-const sendData = () => {};
+const sendData = () => {
+    axios.post('https://reqres.in/api/register', {
+        email: "eve.holt@reqres.in",
+        // password: "pistol"
+    }).then(response => {
+        console.log(response)
+    }, {
+        // headers: {
+        //     'Content-Type': 'application/json'
+        // }
+    })
+        .catch(err => {
+            console.log(err, err.response)
+        })
+}
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
